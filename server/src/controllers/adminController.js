cfd35e533f1b2675b1dfa077f63fd85c3071a326
AnhDuc2k <khungkhip@gmail.com>
2021-12-22 21:18:10 +0700

    token handler

--- server/src/controllers/adminController.js
+++ server/src/controllers/adminController.js
@@ -1,3 +1,41 @@
-exports.helloAnhDuc = (req, res) => {
-  res.send("hello anhduc");
+const CryptoJs = require("crypto-js");
+const jwt = require("jsonwebtoken");
+const {
+  Admin,
+  User,
+  Place,
+  Vaccine,
+  VaccineLot,
+  UserVaccine,
+} = require("../models");
+
+exports.login = async (req, res) => {
+  try {
+    const admin = await Admin.findOne({
+      username: req.body.username,
+    });
+    if (!admin) return res.status(401).json("Wrong username");
+    const decryptedPass = CryptoJs.AES.decrypt(
+      admin.password,
+      process.env.PASSWORD_SECRET_KEY
+    ).toString(CryptoJs.enc.Utf8);
+    if (decryptedPass !== req.body.password)
+      return res.status(401).json("Wrong password");
+
+    const token = jwt.sign(
+      {
+        id: admin._id,
+      },
+      process.env.TOKEN_SECRET_KEY
+    );
+    admin.password = undefined;
+
+    res.status(200).json({
+      token,
+      admin,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 };
--- server/src/routes/adminRoute.js
+++ server/src/routes/adminRoute.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const { adminController } = require("../controllers");
 
-router.get("/", adminController.helloAnhDuc);
-
+router.post("/login", adminController.login);
 module.exports = router;
