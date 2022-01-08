a8a28eea7b7ee14208b86731ff83b7b64f666c23
AnhDuc2k <khungkhip@gmail.com>
2022-01-08 20:34:27 +0700

    admin summary data api

--- server/src/routes/adminRoute.js
+++ server/src/routes/adminRoute.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const { adminController } = require("../controllers");
+const tokenHandler = require("../handlers/tokenHandler");
 
 router.post("/login", adminController.login);
+router.get("/summary", tokenHandler.verifyAdminToken, adminController.summary);
 module.exports = router;
