7c0d4f819975097bb13f01d1ab1f7d28b429e574
AnhDuc2k <khungkhip@gmail.com>
2022-01-28 09:13:44 +0700

    add user place, vaccine and finish pj

--- webApp/src/App.jsx
+++ webApp/src/App.jsx
@@ -3,7 +3,19 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login, AppLayout, Dashboard, User } from "./pages";
+import {
+  Login,
+  AppLayout,
+  Dashboard,
+  User,
+  UserCreate,
+  UserDetail,
+  Vaccine,
+  VaccineDetail,
+  Place,
+  PlaceDetail,
+  QRScan,
+} from "./pages";
 
 function App() {
   return (
@@ -13,6 +25,13 @@ function App() {
         <Route path="" element={<AppLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="/user" element={<User />} />
+          <Route path="/user/create" element={<UserCreate />} />
+          <Route path="/user/:id" element={<UserDetail />} />
+          <Route path="/vaccine" element={<Vaccine />} />
+          <Route path="/vaccine/:id" element={<VaccineDetail />} />
+          <Route path="/place" element={<Place />} />
+          <Route path="/place/:id" element={<PlaceDetail />} />
+          <Route path="/qr-scan" element={<QRScan />} />
         </Route>
       </Routes>
     </BrowserRouter>
